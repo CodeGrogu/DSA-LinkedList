61b7c3be1c691a7c93fb8ec03e11dd22562ba428
Adolf David <143256974+adolfzcoder@users.noreply.github.com>
2024-10-13 09:59:46 +0200

    More initialisation

--- dsa-project-linked-list/src/PhoneBook.java
+++ dsa-project-linked-list/src/PhoneBook.java
@@ -4,5 +4,21 @@ public class PhoneBook {
     
     LinkedList<String> phoneBook = new LinkedList<>();
 
-    
+    public static void displayContacts() {
+        // method to display contacts
+    }
+
+    public static void insertContact(String firstName, String lastName, String phoneNumber) {
+        // method to insert contacts
+    }
+
+    public static void searchContact(String firstName, String lastName) {
+        // method to search contacts
+    }
+
+    public static void deleteContact(String firstName, String lastName) {
+        // method to delete contacts
+    }
+
+
 }
