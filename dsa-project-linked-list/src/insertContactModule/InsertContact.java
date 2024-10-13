6036644a79ee525b67faffdd3e2cdb8a7ae2087a
Rejoice-t <rejoice.teca@gmail.com>
2024-10-13 20:13:44 +0200

    fixing errors 2.0

--- dsa-project-linked-list/src/insertContactModule/InsertContact.java
+++ dsa-project-linked-list/src/insertContactModule/InsertContact.java
@@ -1,8 +1,34 @@
-package insertContactModule;
+//package insertContactModule;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+// import insertContactModule.Contact;
 
 public class InsertContact {
-    public static void insertContact(String firstName, String lastName, String phoneNumber, String email){
-        
+
+    private static LinkedList<Contact> contactList = new LinkedList<>();
+
+    public static void addContact(String firstName, String lastName, String phoneNumber, String email) {
+        Contact newContact = new Contact(firstName, lastName, phoneNumber, email);
+        contactList.add(newContact);
+        System.out.println("Contact added: " + newContact);
+    }
+
+    
+    public void contactsToFile() {
+        try {
+            FileWriter writer = new FileWriter("contacts.csv");
+            writer.write("First Name,Last Name,Phone Number,Email\n"); // CSV headers
+    
+            for (Contact contact : contactList) {
+                writer.write(contact.getFirstName() + "," + contact.getLastName() + "," + contact.getPhoneNumber() + "," + contact.getEmail() + "\n");
+            }
+    
+            writer.close(); // Close the writer to flush data and release resources
+            System.out.println("Contacts saved to contacts.csv");
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle exception if file writing fails
+        }
     }
 }
