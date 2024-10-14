955c7232c08ff75fe0b0588307028068cf27a0cd
Adolf David <143256974+adolfzcoder@users.noreply.github.com>
2024-10-15 00:17:32 +0200

    Removed it printing out twice

--- dsa-project-linked-list/src/searchmodule/searchContact.java
+++ dsa-project-linked-list/src/searchmodule/searchContact.java
@@ -161,22 +161,6 @@ public class searchContact {
         long duration = endTime - startTime;
         System.out.println("Time taken for binary search: " + duration + " nanoseconds");
 
-        if (index >= 0) {
-            String[] contact = contacts.get(index);
-            System.out.println("Contact found: ");
-            System.out.println("Name: " + contact[0] + " " + contact[1]);
-            System.out.println("Email: " + contact[2]);
-            System.out.println("Phone Number: " + contact[3]);
-            System.out.println("Company: " + contact[4]);
-            System.out.println("Job Title: " + contact[5]);
-            System.out.println("City: " + contact[6]);
-            System.out.println("Region: " + contact[7]);
-            System.out.println("Country: " + contact[8]);
-            System.out.println("Notes: " + contact[9]);
-            
-            System.out.println();
-        } else {
-            System.out.println("Contact not found.");
-        }
+    
     }
 }
